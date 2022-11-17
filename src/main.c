dee352ff2f1ff2eea05e4e1164d087ae40e7631e
Mike Szczys <mike@golioth.io>
2022-11-17 11:09:29 -0600

    add support for Golioth logo LED on connect
    Signed-off-by: Mike Szczys <mike@golioth.io>

--- src/main.c
+++ src/main.c
@@ -11,9 +11,14 @@ LOG_MODULE_REGISTER(golioth_hello, LOG_LEVEL_DBG);
 #include <samples/common/net_connect.h>
 #include <zephyr/net/coap.h>
 
+#include <zephyr/drivers/gpio.h>
+
 static struct golioth_client *client = GOLIOTH_SYSTEM_CLIENT_GET();
 
 static K_SEM_DEFINE(connected, 0, 1);
+
+static const struct gpio_dt_spec golioth_led = GPIO_DT_SPEC_GET(
+		DT_ALIAS(golioth_led), gpios);
 
 static void golioth_on_connect(struct golioth_client *client)
 {
@@ -27,6 +32,11 @@ void main(void)
 
 	LOG_DBG("Start Hello sample");
 
+	err = gpio_pin_configure_dt(&golioth_led, GPIO_OUTPUT_INACTIVE);
+	if (err) {
+		LOG_ERR("Unable to configure LED for Golioth Logo");
+	}
+
 	if (IS_ENABLED(CONFIG_GOLIOTH_SAMPLES_COMMON)) {
 		net_connect();
 	}
@@ -35,6 +45,7 @@ void main(void)
 	golioth_system_client_start();
 
 	k_sem_take(&connected, K_FOREVER);
+	gpio_pin_set_dt(&golioth_led, 1);
 
 	while (true) {
 		LOG_INF("Sending hello! %d", counter);
