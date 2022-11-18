a1187bea94c5a9553a45b9a064c89a3f69d47afc
Mike Szczys <mike@golioth.io>
2022-11-18 09:45:34 -0600

    add settings service
    Settings service is used to adjust the LOOP_DELAY_S which is used for
    the main loop sleep. When a new setting is received, the main loop
    wakes, immediately taking a reading, then uses new value for sleep. The
    user button will now also wake the main loop.
    
    Signed-off-by: Mike Szczys <mike@golioth.io>

--- src/main.c
+++ src/main.c
@@ -8,6 +8,7 @@
 LOG_MODULE_REGISTER(golioth_hello, LOG_LEVEL_DBG);
 
 #include <net/golioth/fw.h>
+#include <net/golioth/settings.h>
 #include <net/golioth/system_client.h>
 #include <samples/common/net_connect.h>
 #include <zephyr/net/coap.h>
@@ -19,23 +20,61 @@ static struct golioth_client *client = GOLIOTH_SYSTEM_CLIENT_GET();
 
 K_SEM_DEFINE(connected, 0, 1);
 
+static int32_t _loop_delay_s = 60;
+static k_tid_t _system_thread = 0;
+
 static const struct gpio_dt_spec golioth_led = GPIO_DT_SPEC_GET(
 		DT_ALIAS(golioth_led), gpios);
 static const struct gpio_dt_spec user_btn = GPIO_DT_SPEC_GET(
 		DT_ALIAS(sw1), gpios);
 static struct gpio_callback button_cb_data;
 
+enum golioth_settings_status on_setting(
+		const char *key,
+		const struct golioth_settings_value *value)
+{
+	LOG_DBG("Received setting: key = %s, type = %d", key, value->type);
+	if (strcmp(key, "LOOP_DELAY_S") == 0) {
+		/* This setting is expected to be numeric, return an error if it's not */
+		if (value->type != GOLIOTH_SETTINGS_VALUE_TYPE_INT64) {
+			return GOLIOTH_SETTINGS_VALUE_FORMAT_NOT_VALID;
+		}
+
+		/* This setting must be in range [1, 100], return an error if it's not */
+		if (value->i64 < 1 || value->i64 > 100) {
+			return GOLIOTH_SETTINGS_VALUE_OUTSIDE_RANGE;
+		}
+
+		/* Setting has passed all checks, so apply it to the loop delay */
+		_loop_delay_s = (int32_t)value->i64;
+		LOG_INF("Set loop delay to %d seconds", _loop_delay_s);
+
+		k_wakeup(_system_thread);
+		return GOLIOTH_SETTINGS_SUCCESS;
+	}
+
+	/* If the setting is not recognized, we should return an error */
+	return GOLIOTH_SETTINGS_KEY_NOT_RECOGNIZED;
+}
+
 static void golioth_on_connect(struct golioth_client *client)
 {
 	k_sem_give(&connected);
 
 	app_dfu_observe();
+
+	int err = golioth_settings_register_callback(client, on_setting);
+
+	if (err) {
+		LOG_ERR("Failed to register settings callback: %d", err);
+	}
 }
 
 void button_pressed(const struct device *dev, struct gpio_callback *cb,
 					uint32_t pins)
 {
 	LOG_DBG("Button pressed at %d", k_cycle_get_32());
+	k_wakeup(_system_thread);
 }
 
 void main(void)
@@ -44,6 +83,9 @@ void main(void)
 	int err;
 
 	LOG_DBG("Start Hello sample");
+
+	/* Get system thread id so loop delay change event can wake main */
+	_system_thread = k_current_get();
 
 	err = gpio_pin_configure_dt(&golioth_led, GPIO_OUTPUT_INACTIVE);
 	if (err) {
@@ -91,6 +133,6 @@ void main(void)
 			LOG_WRN("Failed to send hello!");
 		}
 		++counter;
-		k_sleep(K_SECONDS(5));
+		k_sleep(K_SECONDS(_loop_delay_s));
 	}
 }
