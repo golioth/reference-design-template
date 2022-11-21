c320b3baa9436b653e2e9255afe71921292193b8
Mike Szczys <mike@golioth.io>
2022-11-21 14:30:37 -0600

    rpc: use RPC to reboot device
    Signed-off-by: Mike Szczys <mike@golioth.io>

--- src/app_rpc.c
+++ src/app_rpc.c
@@ -11,30 +11,35 @@ LOG_MODULE_REGISTER(app_rpc, LOG_LEVEL_DBG);
 #include <net/golioth/rpc.h>
 #include <qcbor/qcbor.h>
 #include <qcbor/qcbor_spiffy_decode.h>
+#include <zephyr/logging/log_ctrl.h>
+#include <zephyr/sys/reboot.h>
 
-static enum golioth_rpc_status on_multiply(QCBORDecodeContext *request_params_array,
+static void reboot_work_handler(struct k_work *work) {
+	/* Sync longs before reboot */
+	LOG_PANIC();
+
+	for (int8_t i=5; i>=0; i--) {
+		if (i) {
+			LOG_INF("Rebooting in %d seconds...", i);
+		}
+		k_sleep(K_SECONDS(1));
+	}
+
+	sys_reboot(SYS_REBOOT_COLD);
+}
+K_WORK_DEFINE(reboot_work, reboot_work_handler);
+
+static enum golioth_rpc_status on_reboot(QCBORDecodeContext *request_params_array,
 					   QCBOREncodeContext *response_detail_map,
 					   void *callback_arg)
 {
-	double a, b;
-	double value;
-	QCBORError qerr;
+	k_work_submit(&reboot_work);
 
-	QCBORDecode_GetDouble(request_params_array, &a);
-	QCBORDecode_GetDouble(request_params_array, &b);
-	qerr = QCBORDecode_GetError(request_params_array);
-	if (qerr != QCBOR_SUCCESS) {
-		LOG_ERR("Failed to decode array items: %d (%s)", qerr, qcbor_err_to_str(qerr));
-		return GOLIOTH_RPC_INVALID_ARGUMENT;
-	}
-
-	value = a * b;
-	QCBOREncode_AddDoubleToMap(response_detail_map, "value", value);
 	return GOLIOTH_RPC_OK;
 }
 
 int app_register_rpc(struct golioth_client *rpc_client) {
-	int err = golioth_rpc_register(rpc_client, "multiply", on_multiply, NULL);
+	int err = golioth_rpc_register(rpc_client, "reboot", on_reboot, NULL);
 
 	if (err) {
 		LOG_ERR("Failed to register RPC: %d", err);
