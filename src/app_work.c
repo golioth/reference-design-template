9a2cc573061927cd52690c30bf9323ea6ed8d77e
Mike Szczys <mike@golioth.io>
2023-01-11 14:22:58 -0600

    add two mcp3201 ADCs for reading current clamps
    Signed-off-by: Mike Szczys <mike@golioth.io>

--- src/app_work.c
+++ src/app_work.c
@@ -10,12 +10,22 @@ LOG_MODULE_REGISTER(app_work, LOG_LEVEL_DBG);
 #include <net/golioth/system_client.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/sensor.h>
+#include <drivers/spi.h>
+
+#define SPI_OP	SPI_OP_MODE_MASTER | SPI_MODE_CPOL | SPI_MODE_CPHA | SPI_WORD_SET(8) | SPI_LINES_SINGLE
+const struct spi_dt_spec mcp3201_ch0 = SPI_DT_SPEC_GET(DT_NODELABEL(mcp3201_ch0), SPI_OP, 0);
+const struct spi_dt_spec mcp3201_ch1 = SPI_DT_SPEC_GET(DT_NODELABEL(mcp3201_ch1), SPI_OP, 0);
 
 #include "app_work.h"
 #include "libostentus/libostentus.h"
 
 static struct golioth_client *client;
-/* Add Sensor structs here */
+
+/* Store two values for each ADC reading */
+struct mcp3201_data {
+	uint16_t val1;
+	uint16_t val2;
+};
 
 /* Formatting string for sending sensor JSON to Golioth */
 #define JSON_FMT	"{\"counter\":%d}"
@@ -29,11 +39,70 @@ static int async_error_handler(struct golioth_req_rsp *rsp) {
 	return 0;
 }
 
+/*
+ * Validate data received from MCP3201
+ */
+static int process_adc_reading(uint8_t buf_data[4], struct mcp3201_data *adc_data) {
+	if (buf_data[0] & 1<<5) { return -ENOTSUP; }	/* Missing NULL bit */
+	uint16_t data_msb = 0;
+	uint16_t data_lsb = 0;
+	data_msb = buf_data[0] & 0x1F;
+	data_msb |= (data_msb<<7) | (buf_data[1]>>1);
+	for (uint8_t i=0; i<12; i++) {
+		bool bit_set = false;
+		if (i < 2) {
+			if (buf_data[1] & (1<<(1-i))) { bit_set = true; }
+		}
+		else if (i < 10) {
+			if (buf_data[2] & (1<<(2+7-i))) { bit_set = true; }
+		}
+		else {
+			if (buf_data[3] & (1<<(10+7-i))) { bit_set = true; }
+		}
+
+		if (bit_set) { data_lsb |= (1<<i); }
+	}
+
+	adc_data->val1 = data_msb;
+	adc_data->val2 = data_lsb;
+	return 0;
+}
+
 /* This will be called by the main() loop */
 /* Do all of your work here! */
 void app_work_sensor_read(void) {
 	int err;
 	char json_buf[256];
+	struct mcp3201_data adc_data;
+
+	static int8_t my_buffer[4] = {0};
+	struct spi_buf my_spi_buffer[1];
+	my_spi_buffer[0].buf = my_buffer;
+	my_spi_buffer[0].len = 4;
+	const struct spi_buf_set rx_buff = { my_spi_buffer, 1 };
+
+	err = spi_read_dt(&mcp3201_ch0, &rx_buff);
+	if (err) { LOG_INF("spi_read status: %d", err); }
+	LOG_DBG("Received 4 bytes: %d %d %d %d",
+			my_buffer[0],my_buffer[1],my_buffer[2], my_buffer[3]);
+
+	err = process_adc_reading(my_buffer, &adc_data);
+	if (err == 0) {
+		LOG_INF("MCP3201_ch0 received two ADC readings: 0x%04x\t0x%04x",
+				adc_data.val1, adc_data.val2);
+	}
+
+
+	err = spi_read_dt(&mcp3201_ch1, &rx_buff);
+	if (err) { LOG_INF("spi_read status: %d", err); }
+	LOG_DBG("Received 4 bytes: %d %d %d %d",
+			my_buffer[0],my_buffer[1],my_buffer[2], my_buffer[3]);
+
+	err = process_adc_reading(my_buffer, &adc_data);
+	if (err == 0) {
+		LOG_INF("MCP3201_ch1 received two ADC readings: 0x%04x\t0x%04x",
+				adc_data.val1, adc_data.val2);
+	}
 
 	/* For this demo, we just send Hello to Golioth */
 	static uint8_t counter = 0;
@@ -69,5 +138,14 @@ void app_work_sensor_read(void) {
 
 void app_work_init(struct golioth_client* work_client) {
 	client = work_client;
+
+	LOG_DBG("Setting up current clamp ADCs...");
+	LOG_DBG("mcp3201_ch0.bus = %p", mcp3201_ch0.bus);
+	LOG_DBG("mcp3201_ch0.config.cs->gpio.port = %p", mcp3201_ch0.config.cs->gpio.port);
+	LOG_DBG("mcp3201_ch0.config.cs->gpio.pin = %u", mcp3201_ch0.config.cs->gpio.pin);
+	LOG_DBG("mcp3201_ch1.bus = %p", mcp3201_ch1.bus);
+	LOG_DBG("mcp3201_ch1.config.cs->gpio.port = %p", mcp3201_ch1.config.cs->gpio.port);
+	LOG_DBG("mcp3201_ch1.config.cs->gpio.pin = %u", mcp3201_ch1.config.cs->gpio.pin);
+
 }
 
