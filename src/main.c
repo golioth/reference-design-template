2187094d8e5c9ba8119115d4dfb71ed0bb87eb72
Mike Szczys <mike@golioth.io>
2023-02-15 12:03:25 -0600

    update Golioth SDK for observation fix
    This PR updates the Golioth SDK to include a fix for the
    double-observation issue. The workaround has been removed from
    on_connect()
    
    Signed-off-by: Mike Szczys <mike@golioth.io>

--- src/main.c
+++ src/main.c
@@ -38,19 +38,11 @@ static void golioth_on_connect(struct golioth_client *client)
 {
 	k_sem_give(&connected);
 
-	/**
-	 * Workaround for duplicate observations. This will eventually be addressed
-	 * in the Golioth SDK and iteration/conditional can be removed
-	 **/
-	static int iteration = 0;
-	if (iteration > 0) {
-		LOG_INF("Registering observations with Golioth");
-		app_dfu_observe();
-		app_register_settings(client);
-		app_register_rpc(client);
-		app_state_observe();
-	}
-	++iteration;
+	LOG_INF("Registering observations with Golioth");
+	app_dfu_observe();
+	app_register_settings(client);
+	app_register_rpc(client);
+	app_state_observe();
 }
 
 void button_pressed(const struct device *dev, struct gpio_callback *cb,
