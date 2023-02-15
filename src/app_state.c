333379450881b2a833840ba6cf313a9fefad66d4
Mike Szczys <mike@golioth.io>
2023-02-15 10:24:18 -0600

    remove work queue for setting default endpoint
    The "desired" endpoint can be set to defaults using an async call to the
    Golioth LightDB State. A worker is not needed for this process.
    
    Signed-off-by: Mike Szczys <mike@golioth.io>

--- src/app_state.c
+++ src/app_state.c
@@ -21,7 +21,6 @@ uint32_t _example_int1 = 1;
 
 static struct golioth_client *client;
 
-static K_SEM_DEFINE(reset_desired, 0, 1);
 static K_SEM_DEFINE(update_actual, 0, 1);
 
 static int async_handler(struct golioth_req_rsp *rsp)
@@ -38,11 +37,14 @@ static int async_handler(struct golioth_req_rsp *rsp)
 
 void app_state_init(struct golioth_client* state_client) {
 	client = state_client;
-	k_sem_give(&reset_desired);
 	k_sem_give(&update_actual);
 }
 
-static void reset_desired_work_handler(struct k_work *work) {
+static void reset_desired_state() {
+	LOG_INF("Resetting \"%s\" LightDB State endpoint to defaults.",
+			APP_STATE_DESIRED_ENDP
+			);
+
 	char sbuf[strlen(DEVICE_STATE_FMT)+8]; /* small bit of extra space */
 	snprintk(sbuf, sizeof(sbuf), DEVICE_STATE_FMT, -1, -1);
 
@@ -53,10 +55,7 @@ static void reset_desired_work_handler(struct k_work *work) {
 	if (err) {
 		LOG_ERR("Unable to write to LightDB State: %d", err);
 	}
-	k_sem_give(&reset_desired);
 }
-
-K_WORK_DEFINE(reset_desired_work, reset_desired_work_handler);
 
 void app_state_update_actual(void) {
 
@@ -69,21 +68,6 @@ void app_state_update_actual(void) {
 			async_handler, NULL);
 	if (err) {
 		LOG_ERR("Unable to write to LightDB State: %d", err);
-	}
-}
-
-void app_state_observe(void) {
-	int err = golioth_lightdb_observe_cb(client, APP_STATE_DESIRED_ENDP,
-			GOLIOTH_CONTENT_FORMAT_APP_JSON, app_state_desired_handler, NULL);
-	if (err) {
-	   LOG_WRN("failed to observe lightdb path: %d", err);
-	}
-
-	// This will only run when we first connect. It updates the actual state of
-	// the device with the Golioth servers. Future updates will be sent whenever
-	// changes occur.
-	if (k_sem_take(&update_actual, K_NO_WAIT) == 0) {
-		app_state_update_actual();
 	}
 }
 
@@ -102,8 +86,8 @@ int app_state_desired_handler(struct golioth_req_rsp *rsp) {
 			&parsed_state);
 
 	if (ret < 0) {
-		LOG_ERR("Error parsing desired values: %d", -1);
-		k_work_submit(&reset_desired_work);
+		LOG_ERR("Error parsing desired values: %d", ret);
+		reset_desired_state();
 		return 0;
 	}
 
@@ -149,10 +133,23 @@ int app_state_desired_handler(struct golioth_req_rsp *rsp) {
 	if (desired_processed_count) {
 		// We processed some desired changes to return these to -1 on the server
 		// to indicate the desired values were received.
-		if (k_sem_take(&reset_desired, K_NO_WAIT) == 0) {
-			k_work_submit(&reset_desired_work);
-		}
+		reset_desired_state();
 	}
-
 	return 0;
 }
+
+void app_state_observe(void) {
+	int err = golioth_lightdb_observe_cb(client, APP_STATE_DESIRED_ENDP,
+			GOLIOTH_CONTENT_FORMAT_APP_JSON, app_state_desired_handler, NULL);
+	if (err) {
+	   LOG_WRN("failed to observe lightdb path: %d", err);
+	}
+
+	// This will only run when we first connect. It updates the actual state of
+	// the device with the Golioth servers. Future updates will be sent whenever
+	// changes occur.
+	if (k_sem_take(&update_actual, K_NO_WAIT) == 0) {
+		app_state_update_actual();
+	}
+}
+
