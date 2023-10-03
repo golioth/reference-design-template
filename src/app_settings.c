3ac47b29e94f73211320526b886e862a4b4d2cfc
Mike Szczys <mike@golioth.io>
2023-10-03 14:25:13 -0500

    settings: use Golioth Firmware SDK
    Signed-off-by: Mike Szczys <mike@golioth.io>

--- src/app_settings.c
+++ src/app_settings.c
@@ -7,72 +7,42 @@
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(app_settings, LOG_LEVEL_DBG);
 
-#include <net/golioth/settings.h>
-
 #include "main.h"
 #include "app_settings.h"
 
-static struct golioth_client *client;
+static golioth_client_t client;
 
 static int32_t _loop_delay_s = 60;
+#define LOOP_DELAY_S_MAX 43200
+#define LOOP_DELAY_S_MIN 0
 
 int32_t get_loop_delay_s(void)
 {
 	return _loop_delay_s;
 }
 
-enum golioth_settings_status on_setting(const char *key, const struct golioth_settings_value *value)
+static golioth_settings_status_t on_loop_delay_setting(int32_t new_value, void *arg)
 {
-
-	LOG_DBG("Received setting: key = %s, type = %d", key, value->type);
-	if (strcmp(key, "LOOP_DELAY_S") == 0) {
-		/* This setting is expected to be numeric, return an error if it's not */
-		if (value->type != GOLIOTH_SETTINGS_VALUE_TYPE_INT64) {
-			LOG_DBG("Received LOOP_DELAY_S is not an integer type.");
-			return GOLIOTH_SETTINGS_VALUE_FORMAT_NOT_VALID;
-		}
-
-		/* Limit to 12 hour max delay: [1, 43200] */
-		if (value->i64 < 1 || value->i64 > 43200) {
-			LOG_DBG("Received LOOP_DELAY_S setting is outside allowed range.");
-			return GOLIOTH_SETTINGS_VALUE_OUTSIDE_RANGE;
-		}
-
-		/* Only update if value has changed */
-		if (_loop_delay_s == (int32_t)value->i64) {
-			LOG_DBG("Received LOOP_DELAY_S already matches local value.");
-		} else {
-			_loop_delay_s = (int32_t)value->i64;
-			LOG_INF("Set loop delay to %d seconds", _loop_delay_s);
-
-			wake_system_thread();
-		}
-		return GOLIOTH_SETTINGS_SUCCESS;
-	}
-
-	/* If the setting is not recognized, we should return an error */
-	return GOLIOTH_SETTINGS_KEY_NOT_RECOGNIZED;
+	_loop_delay_s = new_value;
+	LOG_INF("Set loop delay to %i seconds", new_value);
+	return GOLIOTH_SETTINGS_SUCCESS;
 }
 
-int app_settings_init(struct golioth_client *state_client)
+int app_settings_init(golioth_client_t settings_client)
 {
-	client = state_client;
+	client = settings_client;
 	int err = app_settings_register(client);
 	return err;
 }
 
-int app_settings_observe(void)
+int app_settings_register(golioth_client_t settings_client)
 {
-	int err = golioth_settings_observe(client);
-	if (err) {
-		LOG_ERR("Failed to observe settings: %d", err);
-	}
-	return err;
-}
-
-int app_settings_register(struct golioth_client *settings_client)
-{
-	int err = golioth_settings_register_callback(settings_client, on_setting);
+	int err = golioth_settings_register_int_with_range(settings_client,
+							   "LOOP_DELAY_S",
+							   LOOP_DELAY_S_MIN,
+							   LOOP_DELAY_S_MAX,
+							   on_loop_delay_setting,
+							   NULL);
 
 	if (err) {
 		LOG_ERR("Failed to register settings callback: %d", err);
