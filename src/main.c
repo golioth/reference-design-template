ac4a753739af616e78ece90e483e4c72416c9f6a
Mike Szczys <mike@golioth.io>
2023-12-22 10:22:46 -0600

    barebones framework for testing Ostentus
    This branch will be used for testing improvemnts to libostentus and the
    Ostentus faceplate itself.
    
    Signed-off-by: Mike Szczys <mike@golioth.io>

--- src/main.c
+++ src/main.c
@@ -7,33 +7,13 @@
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(golioth_rd_template, LOG_LEVEL_DBG);
 
-#include <modem/lte_lc.h>
-#include <net/golioth/system_client.h>
-#include <samples/common/net_connect.h>
-#include <zephyr/net/coap.h>
-#include "app_rpc.h"
-#include "app_settings.h"
-#include "app_state.h"
-#include "app_work.h"
-#include "dfu/app_dfu.h"
+#include "main.h"
 
 #ifdef CONFIG_LIB_OSTENTUS
 #include <libostentus.h>
 #endif
-#ifdef CONFIG_ALUDEL_BATTERY_MONITOR
-#include "battery_monitor/battery.h"
-#endif
 
 #include <zephyr/drivers/gpio.h>
-
-#ifdef CONFIG_MODEM_INFO
-#include <modem/modem_info.h>
-#endif
-
-static struct golioth_client *client = GOLIOTH_SYSTEM_CLIENT_GET();
-
-K_SEM_DEFINE(connected, 0, 1);
-K_SEM_DEFINE(dfu_status_unreported, 1, 1);
 
 static k_tid_t _system_thread = 0;
 
@@ -49,105 +29,6 @@ void wake_system_thread(void)
 	k_wakeup(_system_thread);
 }
 
-static void golioth_on_connect(struct golioth_client *client)
-{
-	k_sem_give(&connected);
-	golioth_connection_led_set(1);
-
-	LOG_INF("Registering observations with Golioth");
-	app_dfu_observe();
-	app_settings_observe();
-	app_rpc_observe();
-	app_state_observe();
-
-	if (k_sem_take(&dfu_status_unreported, K_NO_WAIT) == 0) {
-		/* Report firmware update status on first connect after power up */
-		app_dfu_report_state_to_golioth();
-	}
-}
-
-#ifdef CONFIG_SOC_NRF9160
-static void process_lte_connected(void)
-{
-	/* Change the state of the Internet LED on Ostentus */
-	IF_ENABLED(CONFIG_LIB_OSTENTUS, (led_internet_set(1);));
-
-	golioth_system_client_start();
-}
-
-/**
- * @brief Perform actions based on LTE connection events
- *
- * This is copied from the Golioth samples/common/nrf91_lte_monitor.c to allow us to perform custom
- * actions (turn on LED and start Golioth client) when a network connection becomes available.
- *
- * Set `CONFIG_GOLIOTH_SAMPLE_NRF91_LTE_MONITOR=n` so that the common sample code doesn't collide.
- *
- * @param evt
- */
-static void lte_handler(const struct lte_lc_evt *const evt)
-{
-	switch (evt->type) {
-	case LTE_LC_EVT_NW_REG_STATUS:
-		switch (evt->nw_reg_status) {
-		case LTE_LC_NW_REG_NOT_REGISTERED:
-			LOG_INF("Network: Not registered");
-			break;
-		case LTE_LC_NW_REG_REGISTERED_HOME:
-			LOG_INF("Network: Registered (home)");
-			process_lte_connected();
-			break;
-		case LTE_LC_NW_REG_SEARCHING:
-			LOG_INF("Network: Searching");
-			break;
-		case LTE_LC_NW_REG_REGISTRATION_DENIED:
-			LOG_INF("Network: Registration denied");
-			break;
-		case LTE_LC_NW_REG_UNKNOWN:
-			LOG_INF("Network: Unknown");
-			break;
-		case LTE_LC_NW_REG_REGISTERED_ROAMING:
-			LOG_INF("Network: Registered (roaming)");
-			process_lte_connected();
-			break;
-		case LTE_LC_NW_REG_UICC_FAIL:
-			LOG_INF("Network: UICC fail");
-			break;
-		}
-		break;
-	case LTE_LC_EVT_RRC_UPDATE:
-		switch (evt->rrc_mode) {
-		case LTE_LC_RRC_MODE_CONNECTED:
-			LOG_DBG("RRC: Connected");
-			break;
-		case LTE_LC_RRC_MODE_IDLE:
-			LOG_DBG("RRC: Idle");
-			break;
-		}
-		break;
-	default:
-		break;
-	}
-}
-#endif /* CONFIG_SOC_NRF9160 */
-
-#ifdef CONFIG_MODEM_INFO
-static void log_modem_firmware_version(void)
-{
-	char sbuf[128];
-
-	/* Initialize modem info */
-	int err = modem_info_init();
-
-	if (err) {
-		LOG_ERR("Failed to initialize modem info: %d", err);
-	}
-
-	/* Log modem firmware version */
-	modem_info_string_get(MODEM_INFO_FW_VERSION, sbuf, sizeof(sbuf));
-	LOG_INF("Modem firmware version: %s", sbuf);
-}
-#endif
 
 void button_pressed(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
 {
@@ -172,10 +53,7 @@ int main(void)
 {
 	int err;
 
-	LOG_DBG("Start Reference Design Template sample");
-
-	LOG_INF("Firmware version: %s", CONFIG_MCUBOOT_IMGTOOL_SIGN_VERSION);
-	IF_ENABLED(CONFIG_MODEM_INFO, (log_modem_firmware_version();));
+	LOG_DBG("Start Ostentus test");
 
 	IF_ENABLED(CONFIG_LIB_OSTENTUS, (
 		/* Clear Ostentus memory */
@@ -184,6 +62,7 @@ int main(void)
 		led_bitmask(LED_POW | LED_BAT);
 		/* Show Golioth Logo on Ostentus ePaper screen */
 		show_splash();
+
 	));
 
 	/* Get system thread id so loop delay change event can wake main */
@@ -193,47 +72,6 @@ int main(void)
 	err = gpio_pin_configure_dt(&golioth_led, GPIO_OUTPUT_INACTIVE);
 	if (err) {
 		LOG_ERR("Unable to configure LED for Golioth Logo");
-	}
-
-	/* Initialize app state */
-	app_state_init(client);
-
-	/* Initialize app work */
-	app_work_init(client);
-
-	/* Initialize DFU components */
-	app_dfu_init(client);
-
-	/* Initialize app settings */
-	app_settings_init(client);
-
-	/* Initialize app RPC */
-	app_rpc_init(client);
-
-	/* Register Golioth on_connect callback */
-	client->on_connect = golioth_on_connect;
-
-	/* Start LTE asynchronously if the nRF9160 is used
-	 * Golioth Client will start automatically when LTE connects
-	 */
-	IF_ENABLED(CONFIG_SOC_NRF9160, (LOG_INF("Connecting to LTE, this may take some time...");
-					lte_lc_init_and_connect_async(lte_handler);));
-
-	/* If nRF9160 is not used, start the Golioth Client and block until connected */
-	if (!IS_ENABLED(CONFIG_SOC_NRF9160)) {
-		/* Run WiFi/DHCP if necessary */
-		if (IS_ENABLED(CONFIG_GOLIOTH_SAMPLES_COMMON)) {
-			net_connect();
-		}
-
-		/* Start Golioth client */
-		golioth_system_client_start();
-
-		/* Block until connected to Golioth */
-		k_sem_take(&connected, K_FOREVER);
-
-		/* Turn on Golioth logo LED once connected */
-		gpio_pin_set_dt(&golioth_led, 1);
 	}
 
 	/* Set up user button */
@@ -262,26 +100,25 @@ int main(void)
 		 */
 		slide_add(UP_COUNTER, LABEL_UP_COUNTER, strlen(LABEL_UP_COUNTER));
 		slide_add(DN_COUNTER, LABEL_DN_COUNTER, strlen(LABEL_DN_COUNTER));
-		IF_ENABLED(CONFIG_ALUDEL_BATTERY_MONITOR, (
-			slide_add(BATTERY_V, LABEL_BATTERY, strlen(LABEL_BATTERY));
-			slide_add(BATTERY_LVL, LABEL_BATTERY, strlen(LABEL_BATTERY));
-		));
-		slide_add(FIRMWARE, LABEL_FIRMWARE, strlen(LABEL_FIRMWARE));
 
 		/* Set the title ofthe Ostentus summary slide (optional) */
 		summary_title(SUMMARY_TITLE, strlen(SUMMARY_TITLE));
-
-		/* Update the Firmware slide with the firmware version */
-		slide_set(FIRMWARE, CONFIG_MCUBOOT_IMGTOOL_SIGN_VERSION,
-			  strlen(CONFIG_MCUBOOT_IMGTOOL_SIGN_VERSION));
 
 		/* Start Ostentus slideshow with 30 second delay between slides */
 		slideshow(30000);
 	));
 
-	while (true) {
-		app_work_sensor_read();
+	char json_buf[32];
+	uint8_t counter = 0;
 
-		k_sleep(K_SECONDS(get_loop_delay_s()));
+	while (true) {
+
+		snprintk(json_buf, sizeof(json_buf), "%d", counter);
+		slide_set(UP_COUNTER, json_buf, strlen(json_buf));
+		snprintk(json_buf, sizeof(json_buf), "%d", 255 - counter);
+		slide_set(DN_COUNTER, json_buf, strlen(json_buf));
+		counter++;
+
+		k_sleep(K_SECONDS(1));
 	}
 }
--- src/main.h
+++ src/main.h
@@ -5,3 +5,16 @@
  */
 
 void wake_system_thread(void);
+
+#define LABEL_UP_COUNTER "Counter"
+#define LABEL_DN_COUNTER "Anti-counter"
+#define SUMMARY_TITLE	 "Counters:"
+
+/**
+ * Each Ostentus slide needs a unique key. You may add additional slides by
+ * inserting elements with the name of your choice to this enum.
+ */
+typedef enum {
+	UP_COUNTER,
+	DN_COUNTER,
+} slide_key;
