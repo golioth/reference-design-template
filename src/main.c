b7f92b94199df50e950d86e4468fd7a4d94849ab
Mike Szczys <mike@golioth.io>
2023-12-31 12:13:34 -0600

    Add test to see if FIFO is full
    * Break out the Ostentus initialiation blocks into functions
    * Break out the Ostentus test into a function
    * When testing reset, first test filling the FIFO
    * After reset, test reading the FIFO as an indication that Ostentus is
      ready
    
    Signed-off-by: Mike Szczys <mike@golioth.io>

--- src/main.c
+++ src/main.c
@@ -16,6 +16,7 @@ LOG_MODULE_REGISTER(golioth_rd_template, LOG_LEVEL_DBG);
 #include <zephyr/drivers/gpio.h>
 
 static k_tid_t _system_thread = 0;
+uint8_t counter;
 bool button_reset_pressed;
 
 static const struct gpio_dt_spec golioth_led = GPIO_DT_SPEC_GET(DT_ALIAS(golioth_led), gpios);
@@ -51,20 +52,66 @@ void golioth_connection_led_set(uint8_t state)
 	IF_ENABLED(CONFIG_LIB_OSTENTUS, (led_golioth_set(pin_state);));
 }
 
+void init_ostentus(void)
+{
+	/* Clear Ostentus memory */
+	clear_memory();
+	/* Update Ostentus LEDS using bitmask (Power On and Battery) */
+	led_bitmask(LED_POW | LED_BAT);
+	/* Show Golioth Logo on Ostentus ePaper screen */
+	show_splash();
+}
+
+void init_slideshow(void)
+{
+	/* Set up a slideshow on Ostentus
+	 *  - add up to 256 slides
+	 *  - use the enum in app_work.h to add new keys
+	 *  - values are updated using these keys (see app_work.c)
+	 */
+	slide_add(UP_COUNTER, LABEL_UP_COUNTER, strlen(LABEL_UP_COUNTER));
+	slide_add(DN_COUNTER, LABEL_DN_COUNTER, strlen(LABEL_DN_COUNTER));
+
+	/* Set the title ofthe Ostentus summary slide (optional) */
+	summary_title(SUMMARY_TITLE, strlen(SUMMARY_TITLE));
+
+	/* Start Ostentus slideshow with 30 second delay between slides */
+	slideshow(30000);
+}
+
+void test_fifo_and_reset(void)
+{
+	/* Test filling up the FIFO */
+	uint8_t slots = 1;
+
+	while (slots) {
+		show_splash();
+		ostentus_fifo_ready(&slots);
+		LOG_INF("FIFO slots remaining: %d", slots);
+	}
+
+	/* Tests finished, reset now */
+	counter = 0;
+	button_reset_pressed = false;
+	ostentus_reset();
+
+	/* Wait for Ostentus to come back up */
+	while (ostentus_fifo_ready(&slots)) {
+		k_msleep(100);
+	}
+
+	/* Reinit */
+	init_ostentus();
+	init_slideshow();
+}
+
 int main(void)
 {
 	int err;
 
 	LOG_DBG("Start Ostentus test");
 
-	IF_ENABLED(CONFIG_LIB_OSTENTUS, (
-		/* Clear Ostentus memory */
-		clear_memory();
-		/* Update Ostentus LEDS using bitmask (Power On and Battery) */
-		led_bitmask(LED_POW | LED_BAT);
-		/* Show Golioth Logo on Ostentus ePaper screen */
-		show_splash();
-	));
+	init_ostentus();
 
 	/* Get system thread id so loop delay change event can wake main */
 	_system_thread = k_current_get();
@@ -93,35 +140,19 @@ int main(void)
 	gpio_init_callback(&button_cb_data, button_pressed, BIT(user_btn.pin));
 	gpio_add_callback(user_btn.port, &button_cb_data);
 
-	IF_ENABLED(CONFIG_LIB_OSTENTUS,(
-		/* Set up a slideshow on Ostentus
-		 *  - add up to 256 slides
-		 *  - use the enum in app_work.h to add new keys
-		 *  - values are updated using these keys (see app_work.c)
-		 */
-		slide_add(UP_COUNTER, LABEL_UP_COUNTER, strlen(LABEL_UP_COUNTER));
-		slide_add(DN_COUNTER, LABEL_DN_COUNTER, strlen(LABEL_DN_COUNTER));
-
-		/* Set the title ofthe Ostentus summary slide (optional) */
-		summary_title(SUMMARY_TITLE, strlen(SUMMARY_TITLE));
-
-		/* Start Ostentus slideshow with 30 second delay between slides */
-		slideshow(30000);
-	));
+	init_slideshow();
 
 	char json_buf[32];
-	uint8_t counter = 0;
 
 	ostentus_version_get(json_buf, sizeof(json_buf));
 	LOG_INF("Ostentus Firmware Version: %s", json_buf);
 
 	while (true) {
+		/* Reset when button is pressed or all lights but Golioth are on */
 		if ((button_reset_pressed) || (counter > 0x1D)) {
-			/* Reset when all lights but Golioth are on */
-			counter = 0;
-			button_reset_pressed = false;
-			ostentus_reset();
+			test_fifo_and_reset();
 		}
+
 		led_bitmask(counter);
 		snprintk(json_buf, sizeof(json_buf), "%d", counter);
 		slide_set(UP_COUNTER, json_buf, strlen(json_buf));
