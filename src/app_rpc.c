c5464ef2aa74c850e3e20c2e80015f7008d1cd94
Mike Szczys <mike@golioth.io>
2025-03-12 15:59:30 -0500

    handle Nordic-specific config/code
    - Move MBEDTLS_SSL_IN_CONTENT_LEN and MBEDTLS_SSL_OUT_CONTENT_LEN to
      nrf9160_ns.conf as it is only used by NRF_SECURITY
    - The network info library only supports Nordic devices. Enable it in
      nrf9160_ns.conf and use Kconfig to conditionally build the function calls
    
    Signed-off-by: Mike Szczys <mike@golioth.io>

--- src/app_rpc.c
+++ src/app_rpc.c
@@ -12,7 +12,10 @@ LOG_MODULE_REGISTER(app_rpc, LOG_LEVEL_DBG);
 #include <zephyr/logging/log_ctrl.h>
 #include <zephyr/sys/reboot.h>
 
+#ifdef CONFIG_NETWORK_INFO
 #include <network_info.h>
+#endif
+
 #include "app_rpc.h"
 
 static void reboot_work_handler(struct k_work *work)
@@ -35,9 +38,9 @@ static enum golioth_rpc_status on_get_network_info(zcbor_state_t *request_params
 						   zcbor_state_t *response_detail_map,
 						   void *callback_arg)
 {
-	network_info_add_to_map(response_detail_map);
-
-	return GOLIOTH_RPC_OK;
+	COND_CODE_1(CONFIG_NETWORK_INFO,
+		    (network_info_add_to_map(response_detail_map); return GOLIOTH_RPC_OK;),
+		    (return GOLIOTH_RPC_UNIMPLEMENTED););
 }
 
 static enum golioth_rpc_status on_set_log_level(zcbor_state_t *request_params_array,
